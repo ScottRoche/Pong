46352bdd9c6e5da38fbab54a2c4aeb4fb83f8007
Scott Roche <scott.roche24@gmail.com>
2024-07-14 16:01:31 +0100

    Implement paddle and ball entities for local multiplayer
    This change adds paddles and ball entities to the game. The paddles
    are individually controllable one with W and S, the other with up arrow
    and down arrow. The first game mode will be local multiplayer which is
    why both paddles are controllable with the same keyboard.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,97 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, color::palettes::basic::WHITE};
 
-fn init() {
-    info!("Starting");
+struct KeyMap {
+    up: KeyCode,
+    down: KeyCode
 }
 
-fn update() {
+#[derive(Component)]
+struct Ball;
+
+#[derive(Component)]
+struct PlayerController {
+    keymap: KeyMap
+}
+
+impl PlayerController {
+    fn translate(&self,
+                 delta_time: f32,
+                 input: &Res<ButtonInput<KeyCode>>,
+                 transform: &mut Transform) {
+        if input.pressed(self.keymap.up) {
+            transform.translation.y += 150. * delta_time;
+        }
+
+        if input.pressed(self.keymap.down) {
+            transform.translation.y -= 150. * delta_time;
+        }
+    }
+}
+
+fn init(mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<ColorMaterial>>) {
+    info!("Starting");
+
+    commands.spawn(Camera2dBundle {
+        camera: Camera {
+            clear_color: ClearColorConfig::Custom(Color::srgb(0., 0., 0.)),
+            ..default()
+        },
+        ..default()
+    });
+
+    let player1_position: Vec3 = Vec3::new(64., 0., 0.);
+    let player1_paddle = MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        transform: Transform::default()
+            .with_translation(player1_position)
+            .with_scale(Vec3::new(16., 128., 0.)),
+        material: materials.add(Color::from(WHITE)),
+        ..default()
+    };
+    commands.spawn((player1_paddle,
+                    PlayerController {
+                        keymap: KeyMap {
+                            up: KeyCode::ArrowUp,
+                            down: KeyCode::ArrowDown,
+                        },
+                    }));
+
+    let player2_position: Vec3 = Vec3::new(-64., 0., 0.);
+    let player2_paddle = MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        transform: Transform::default()
+            .with_translation(player2_position)
+            .with_scale(Vec3::new(16., 128., 0.)),
+        material: materials.add(Color::from(WHITE)),
+        ..default()
+    };
+
+    commands.spawn((player2_paddle,
+                    PlayerController {
+                        keymap: KeyMap {
+                            up: KeyCode::KeyW,
+                            down: KeyCode::KeyS,
+                        },
+                    }));
+
+    let ball = MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        transform: Transform::default()
+            .with_scale(Vec3::splat(16.)),
+        material: materials.add(Color::from(WHITE)),
+        ..default()
+    };
+    commands.spawn((ball, Ball));
+}
+
+fn update(time: Res<Time>,
+          keyboard_input: Res<ButtonInput<KeyCode>>,
+          mut query: Query<(&PlayerController, &mut Transform)>) {
+    for (controller, mut transform) in &mut query {
+        controller.translate(time.delta_seconds(), &keyboard_input, &mut transform)
+    }
 }
 
 fn main() {
