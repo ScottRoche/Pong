58d98d5f8368736c8e2257a4b9aed989850a5771
Scott Roche <scott.roche24@gmail.com>
2024-07-21 15:08:16 +0100

    Add collision detection for balls and paddles
    Adds collision detection using the avian2d libary's ColliderAabb, and
    correct the trajectory of the ball when a collision is made. This is
    done by changing the values in the velocity component of the ball.
    
    Now that I think about it the velocities direction should be a normal
    vector in order for the translation to be consistent in all directions.
    This is something that will need to be added later.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, color::palettes::basic::WHITE};
+use bevy::{color::palettes::basic::WHITE, prelude::*, sprite::MaterialMesh2dBundle};
+use avian2d::prelude::*;
 
 struct KeyMap {
     up: KeyCode,
@@ -7,6 +8,12 @@ struct KeyMap {
 
 #[derive(Component)]
 struct Ball;
+
+#[derive(Component)]
+struct Velocity {
+    speed: f32,
+    direction: Vec2,
+}
 
 #[derive(Component)]
 struct PlayerController {
@@ -83,10 +90,16 @@ fn init(mut commands: Commands,
         material: materials.add(Color::from(WHITE)),
         ..default()
     };
-    commands.spawn((ball, Ball));
+    commands.spawn((ball,
+                    Velocity {
+                        speed: 100.,
+                        direction: Vec2::new(1., 1.)
+                    },
+                    Ball));
 }
 
-fn update(time: Res<Time>,
+
+fn paddles_move(time: Res<Time>,
           keyboard_input: Res<ButtonInput<KeyCode>>,
           mut query: Query<(&PlayerController, &mut Transform)>) {
     for (controller, mut transform) in &mut query {
@@ -94,10 +107,90 @@ fn update(time: Res<Time>,
     }
 }
 
+fn ball_move(time: Res<Time>, mut ball_query: Query<(&Velocity, &mut Transform), With<Ball>>) {
+    let (velocity, mut transform) = ball_query.single_mut();
+    transform.translation.x += time.delta_seconds() * velocity.speed * velocity.direction.x;
+    transform.translation.y += time.delta_seconds() * velocity.speed * velocity.direction.y;
+}
+
+enum CollisionSide {
+    Top,
+    Bottom,
+    Left,
+    Right
+}
+
+fn calculate_collision_side(effector: ColliderAabb, effected: ColliderAabb) -> CollisionSide
+{
+    let offset = effector.center() - effected.center(); /* I know with this how far away the
+                                                         * centres of the colliders are. */
+    let offset_distance = offset.abs().ceil();
+
+    // we need to know where on the effected collider that the effector hit.
+    // i.e. if the x scale of the effected and effector = the x value of the offset we are on a
+    // side.
+
+    let offset_scale = (effector.size() + effected.size()) / 2.;
+
+    let side = if offset_scale.x == offset_distance.x {
+        if offset_scale.x < 0. {
+            CollisionSide::Left
+        }
+        else {
+            CollisionSide::Right
+        }
+    }
+    else if offset_scale.y == offset_distance.y {
+        if offset_scale.y < 0. {
+            CollisionSide:: Bottom
+        }
+        else {
+            CollisionSide::Top
+        }
+    }
+    else {
+        warn!("offset: {} {}", offset.x, offset.y);
+        warn!("effector size: {} {}", effector.size().x, effector.size().y);
+        warn!("effected size: {} {}", effected.size().x, effected.size().y);
+        warn!("offset scale: {} {}", offset_scale.x, offset_scale.y);
+        panic!();
+    };
+
+    side
+}
+
+fn detect_collision(mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
+                    collider_query: Query<&Transform, With<PlayerController>>) {
+    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+    let ball_collider = ColliderAabb::new(ball_transform.translation.truncate(), ball_transform.scale.truncate() / 2.);
+
+    debug!("Colliders: {}", collider_query.iter().len());
+
+    for transform in &collider_query {
+        debug!("Checking collision");
+        let collider = ColliderAabb::new(transform.translation.truncate(), transform.scale.truncate() / 2.);
+        if !collider.intersects(&ball_collider) {
+            debug!("No collision detected");
+            continue;
+        }
+
+        debug!("Collision detected");
+
+        // update balls velocity after the collision
+        let impact_side: CollisionSide = calculate_collision_side(ball_collider, collider);
+
+        // reflect the velocity based on the collision side
+        match impact_side {
+            CollisionSide::Top | CollisionSide::Bottom => ball_velocity.direction.y = -ball_velocity.direction.y,
+            CollisionSide::Right | CollisionSide::Left => ball_velocity.direction.x = -ball_velocity.direction.x,
+        }
+    }
+}
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .add_systems(Startup, init)
-        .add_systems(Update, update)
+        .add_systems(Update, (ball_move, paddles_move, detect_collision).chain())
         .run();
 }
